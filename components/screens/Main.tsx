c716de4a408eed0ef5c849efcb7677af149635e0
GeraldineRomer <geraldine.rome0104@gmail.com>
2024-09-02 22:02:03 -0500

    creation of emergency button [atoms]

--- components/screens/Main.tsx
+++ components/screens/Main.tsx
@@ -1,11 +1,13 @@
 import { Text, View } from "react-native"
 import { Link } from "expo-router"
+import EmergencyButton from "../atoms/EmergencyButton"
 
 export default function Main() {
     return (
         <View>
-            <Text style={{color: 'black'}}>Main</Text>
-            <Link href="/login">Login</Link>
+            {/* <Text style={{color: 'black'}}>Main</Text>
+            <Link href="/login">Login</Link> */}
+            <EmergencyButton/>
         </View>
     )
 }
