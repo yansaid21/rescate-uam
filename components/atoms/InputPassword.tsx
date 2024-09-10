c7eb79ba572c1b967eae4ff66eb61d6903794e45
GeraldineRomer <geraldine.rome0104@gmail.com>
2024-09-10 00:10:42 -0500

    Create a screen of login. Creation of atomic components of inputs and buttons. TODO: logo for password input and paths for 'accept' button and 'register' text.

---
+++ components/atoms/InputPassword.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { TextInput, View, StyleSheet, TouchableOpacity } from 'react-native';
+import { EyeIcon } from '../atoms/icons';
+
+export default function InputPassword() {
+    const [isPasswordVisible, setPasswordVisible] = useState(false);
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.inputContainer}>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Contraseña"
+                    placeholderTextColor={'#000000'}
+                    secureTextEntry={!isPasswordVisible}  // Controla si la contraseña es visible o no
+                />
+                <TouchableOpacity onPress={() => setPasswordVisible(!isPasswordVisible)}>
+                    <EyeIcon
+                        size={24}
+                        color="white"
+                    />
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+    },
+    inputContainer: {
+        flexDirection: 'row',  // Para alinear el TextInput y el ícono en la misma línea
+        alignItems: 'center',
+    },
+    input: {
+        height: 48,
+        width: 280,
+        borderRadius: 20,
+        backgroundColor: '#D9D9D9',
+        paddingHorizontal: 15,
+    },
+});
--- components/screens/Login.tsx
+++ components/screens/Login.tsx
@@ -1,16 +1,31 @@
-import { View, Text, Image, StyleSheet } from "react-native"
+import { View, Text, Image, StyleSheet, TextInput, TouchableOpacity } from "react-native"
 import { useSafeAreaInsets } from "react-native-safe-area-context"
 import GoogleButton from "../atoms/GoogleButton";
+import CustomButton from "../atoms/CustomButton";
+import InputPassword from "../atoms/InputPassword";
+import { Link } from "expo-router";
+import Input from "../atoms/Input";
+
 
 export default function Login() {
     const insets = useSafeAreaInsets();
     return (
-        <View style={{paddingTop: insets.top, paddingBottom: insets.bottom}}>
+        <View style={styles.container}>
             <Image
-            style={styles.logo}
-            source={require('../../assets/UAM/Logos_UAM-07.png')}/>
-{/*             <Text style={{color: 'white'}}>Here is login</Text> */}
+                style={styles.logo}
+                source={require('../../assets/UAM/Logos_UAM-07.png')}
+            />
+            <Text style={styles.title}>Bienvenido a Rescates UAM</Text>
+            <Input text="Correo"/>
+            <InputPassword/>
+            <CustomButton 
+                text="Aceptar" 
+                customFun={() => alert('Button Pressed!')} 
+                color="#0069A3" 
+            />
+            <Text style={styles.text}>Entrar con</Text>
             <GoogleButton/>
+            <Link href='/Register' style={styles.register}>Registrarse</Link>
         </View>
     )
 }
@@ -18,9 +33,40 @@ export default function Login() {
 const styles= StyleSheet.create({
     container:{
             flex:1,
-            justifyContent:"center",
+            justifyContent:"space-between",
+            flexDirection: "column",
+            alignItems: 'center',
+            margin: 20
     },
     logo:{
-    width:"auto",
-    height:60}
+        width:"auto",
+        height: 100
+    },
+    title:{
+        fontFamily: 'Fira Sans',
+        fontSize: 36,
+        textAlign: 'center',
+        color: '#0090D0'
+    },
+    input: {
+        height: 48,
+        width: 280,
+        borderRadius: 20,
+        backgroundColor: '#D9D9D9',
+        paddingHorizontal: 15,
+    },
+    btn:{
+        justifyContent: 'center'
+    },
+    text:{
+        fontSize: 18,
+        textAlign: 'center',
+        color: '#BDBDBD'
+    },
+    register:{
+        fontSize: 18,
+        textAlign: 'center',
+        color: '#BDBDBD',
+        textDecorationLine: 'underline'
+    }
 })
