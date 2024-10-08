269381c2719288205668e8f0425b4c97e2078bdf
JuanC2004 <juanc.quinteroh@autonoma.edu.co>
2024-10-08 02:20:29 -0500

    Put method conected with backend (ONLY MAKE CHANGES IN THE DATABASE WHEN ALL THE INPUTS ARE DIFERENT) [put.ts and EditProfile.tsx]

--- components/screens/EditProfile.tsx
+++ components/screens/EditProfile.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { ProfileIcon } from '../atoms/Icons';
+import { StyleSheet, Text, View, Alert } from 'react-native';
 import Input from '../atoms/Input';
 import CustomButton from '../atoms/CustomButton';
 import { getUserInfo } from '../../auth/get';
+import { updateUserInfo } from '../../auth/put'; // Importa la función de actualización
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const EditProfile = () => {
@@ -34,35 +34,62 @@ const EditProfile = () => {
         };
 
         fetchUserInfo();
-    }, []); 
+    }, []);
+
+    const handleSave = async () => {
+        try {
+            const token = await AsyncStorage.getItem('token'); 
+            const id_user = await AsyncStorage.getItem('id'); 
+
+            if (token && id_user) {
+                const userData = {
+                    email,
+                    name,
+                    last_name: lastname,
+                    id_card: id
+                };
+
+                const response = await updateUserInfo(Number(id_user), token, userData); 
+                
+                if (response) {
+                    Alert.alert("Éxito", "Perfil actualizado correctamente");
+                } else {
+                    Alert.alert("Error", "No se pudo actualizar el perfil");
+                }
+            }
+        } catch (error) {
+            Alert.alert("Error", "Hubo un problema al actualizar el perfil");
+            console.error("Error updating user info:", error);
+        }
+    };
 
     return (
         <View className='flex-1 flex-col justify-between items-center m-5'>
-            {/* <ProfileIcon size={200} color='#000' /> */}
             <Text className='font-medium text-center text-[24px] m-5'>Editar perfil</Text>
             <Input 
                 text="Correo"
                 value={email}
                 onChangeText={setEmail}    
-                />
+            />
             <Input 
                 text="Nombre"
                 value={name}
                 onChangeText={setName}    
-                />
+            />
             <Input 
                 text="Apellido"
                 value={lastname}
                 onChangeText={setLastname}    
-                />
+            />
             <Input 
                 text="Identificación"
                 value={id}
                 onChangeText={setId}    
-                />
+            />
             <CustomButton 
                 text="Guardar" 
-                />
+                onPress={handleSave}  // Llama a handleSave al presionar el botón
+            />
         </View>
     );
 }
@@ -70,4 +97,3 @@ const EditProfile = () => {
 const styles = StyleSheet.create({})
 
 export default EditProfile;
-
