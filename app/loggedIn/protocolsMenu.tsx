265462fd5234ade738165d5193a45e218613346d
JuanC2004 <juanc.quinteroh@autonoma.edu.co>
2024-11-25 10:17:46 -0500

    Creation of protocols menu [ProtocolsMenu.tsx, RisksMenu.tsx]

---
+++ app/loggedIn/protocolsMenu.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+import ProtocolsMenu from "../../components/screens/ProtocolsMenu";
+
+const protocolsMenu = () => {
+    return(
+        <ProtocolsMenu/>
+    );
+}
+
+const styles = StyleSheet.create({});
+
+export default ProtocolsMenu;
--- utils/constants.ts
+++ utils/constants.ts
@@ -1,2 +1,2 @@
-export const SERVER_IP = 'rescueapi.xyz';
+export const SERVER_IP = '192.168.1.72';
 //jean's hose ip= 192.168.1.12
